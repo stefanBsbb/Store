3fa799323d82a829679f639f31c4bef43d37f182
stefanBsbb <52064693+stefanBsbb@users.noreply.github.com>
2022-01-11 00:13:37 +0200

    Initial commit

---
+++ Store/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    public  class Products
+    {
+        public Appliances Appliances { get; set; }
+        public Clothes Clothes { get; set; }
+        public Beverages Beverages { get; set; }
+        public Food Food { get; set; }
+    }
+}
